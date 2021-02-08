2eef501cc4e3a116a72dc70dfe8bd1c5e4b92234
Chase Gruber <cgruber@esri.com>
2021-02-08 18:45:08 -0500

    cleaning up

--- client/browser.js
+++ client/browser.js
@@ -6,8 +6,7 @@ const url = new URL(window.location.href);
 const env = url.searchParams.has('env') ? url.searchParams.get('env').toLocaleLowerCase() : 'qa';
 const user = url.searchParams.get('user');
 const secret = window.localStorage.getItem('token4-secret');
-alert('hey hi');
+
 token4(env, user, secret).then(token => {
-  alert('hello');
-  window.localStorage.setItem('token', token);
+  window.localStorage.setItem('bearer-token', token);
 });
