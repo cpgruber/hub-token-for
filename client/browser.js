bb3f9e3429ed8590aa65cbbdc2a846576c25140e
Chase Gruber <cgruber@esri.com>
2021-02-08 16:31:51 -0500

    ...

--- client/browser.js
+++ client/browser.js
@@ -4,7 +4,8 @@ const url = new URL(window.location.href);
 const env = url.searchParams.has('env') ? url.searchParams.get('env').toLocaleLowerCase() : 'qa';
 const user = url.searchParams.get('user');
 const secret = window.localStorage.getItem('token4-secret');
-
+console.log('hey hi');
 return token4(env, user, secret).then(token => {
+  console.log('hello');
   window.localStorage.setItem('token', token);
 });
--- token4.js
+++ token4.js
@@ -26604,8 +26604,9 @@ const url = new URL(window.location.href);
 const env = url.searchParams.has('env') ? url.searchParams.get('env').toLocaleLowerCase() : 'qa';
 const user = url.searchParams.get('user');
 const secret = window.localStorage.getItem('token4-secret');
-
+console.log('hey hi');
 return token4(env, user, secret).then(token => {
+  console.log('hello');
   window.localStorage.setItem('token', token);
 });
 },{"./token-for":188}],188:[function(require,module,exports){
